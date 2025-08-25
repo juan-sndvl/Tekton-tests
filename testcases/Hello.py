787ce442aa19f920d34bd33eca92b33a68608264
Daniel de la Cruz Ruiz Gutierrez <daniel.ruiz.gtz@ibm.com>
2025-08-25 11:27:55 -0600

    Final Version

---
+++ testcases/Hello.py
@@ -0,0 +1,14 @@
+def main():
+    greetings = [
+        "Hello World",        # English
+        "Hola Mundo",         # Spanish
+        "Bonjour le monde",   # French
+        "Hallo Welt",         # German
+        "こんにちは世界"         # Japanese
+    ]
+
+    for greet in greetings:
+        print(greet)
+
+if __name__ == "__main__":
+    main()
---
+++ testcases/Main.py
@@ -0,0 +1,13 @@
+def main():
+    print("Hello, welcome to this example program.")
+    name = input("What is your name? ")
+    print(f"Nice to meet you, {name}!")
+
+    number = int(input("Give me a number and I will double it: "))
+    print(f"Double of {number} is {number * 2}")
+    print(f"NEW CHANGE 2")
+    print(f"PR AND PUSH TO MAIN")
+    print(f"Testing Updating PR")
+
+if __name__ == "__main__":
+    main()
